4d5aa7e0ece775fae5dd7980c69f8417d79a4cf9
ayush <ayushsinghasg71@gmail.com>
2026-03-05 10:21:57 +0530

    Added Garbage Collector file

---
+++ OOPS_concept/GarbageCollector.java
@@ -0,0 +1,18 @@
+package java_learnings.OOPS_concept;
+
+public class GarbageCollector {
+    static int count = 0;
+
+    public void finalize() { // finalize method is called by the garbage collector before it destroys the object.
+        count++;
+        // System.out.println("Garbage Collector called " + count + " times");
+    }
+    public static void main(String[] args) {
+        for(int i = 0; i < 10000000; i++) {
+            GarbageCollector obj = new GarbageCollector();
+            obj = null; // we are making the reference variable null so that it can be eligible for garbage collection.
+
+
+        }
+    }
+}
